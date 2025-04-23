34e15a85e9d7a4f15d1aa653e5a187179673508d
Renan Breier <renanoliveirabreier@gmail.com>
2025-04-23 20:06:41 -0300

    Correção da tela de Perfil

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:datingapp/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'; // <- necessário para suporte a localizações
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +13,23 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // remove o banner de debug
       title: 'Dating App',
-      home: LoginScreen()
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const LoginScreen(),
+
+      // Adiciona suporte a localizações para o DatePicker e outros widgets
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+        Locale('en', 'US'),
+      ],
     );
   }
 }
